eac8a2670e3987c9e4e330ab055a4478b64bd238
your name <luonghuy148894@gmail.com>
2023-07-03 18:08:57 +0700

    create_table_model_migration

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -2,9 +2,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@example.com',
+      password: 'luonghuy2k2',
+      firstName: 'Luong',
+      lastName: 'Huy',
+      address: 'Hoidanit',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
